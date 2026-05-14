132882a97d25584d1c11c7f1bee2378073bacdd2
sam-evolv <sam@evolvai.ie>
2026-05-14 15:53:55 +0100

    Allow Supabase and Mapbox images only

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: '**.supabase.in' },
-      { protocol: 'https', hostname: 'images.unsplash.com' }
+      { protocol: 'https', hostname: 'api.mapbox.com' }
     ]
   }
 };
